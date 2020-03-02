55bd1a5dddd178d21a21db6579dd8c2348ec5380
valeriaosoriou <you@example.comvaleria.osoriou@gmail.com>
2020-03-02 02:26:40 +0000

    data from Mackaroo working

---
+++ README.md
@@ -0,0 +1,1 @@
+git clone https://github.com/valeriaosoriou/express-project.git
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();//top-level function of express
 const path = require('path');
 const apiData = require('./people.json');
 
+const cars = require('./cars.json');
+const carOwners = require('./car-owners.json');
+
 const port = 3000;
 
 app.use((req,res,next)=>{
@@ -19,6 +22,7 @@ app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstra
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/popper', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
 app.use('/js', express.static(path.join(__dirname,'public/js')));
+app.use('/css', express.static(path.join(__dirname,'public/css')));
 
 // set the route for index.html
 app.get('/', (req,res,)=>{
@@ -29,10 +33,72 @@ app.get('/', (req,res,)=>{
 app.get('/about', (req,res,)=>{
   res.sendFile(path.join(__dirname+'/public/about.html'));
 });
+
 //give access to apiData
 app.get('/people', (req,res,)=>{
   res.json(apiData);
 });
 
+//api gender
+app.get('/gender/g=:gender', (req,res,)=>{
+	const genderParam = req.params.gender
+	if ((genderParam === 'male') || (genderParam === 'female')){
+		let filteredArray = [];//array to push the matching objects to user's value
+		for (let i = 0; i < apiData.length; i++){
+			if (genderParam === apiData[i].gender.toLowerCase()){
+				filteredArray.push(apiData[i]);
+			}
+		}//for 
+		res.send(filteredArray);
+	}else {
+		res.send('Invalid parameter');
+	
+	}//if
+});//gender
+
+app.get('/cars/make=:make&model=:model',(req,res)=>{
+  const makeParam = req.params.make;
+  const modelParam = req.params.model; //retrieves the parameter value requested by the user
+  // if ((modelParam === 'male') || (genderParam === 'female')){
+    let filteredArray = [];//array to push the matching objects to user's value
+    for (let i = 0; i < cars.length; i++) {
+      if ((modelParam.toLowerCase() === cars[i].car_model.toLowerCase()) && (makeParam.toLowerCase() === cars[i].car_make.toLowerCase())){
+        filteredArray.push(cars[i]);
+      }
+    }
+    res.send(filteredArray);
+
+});
+
+
+app.get('/carowners/ownscar=:owns_car',(req,res)=>{
+  const ownsParam = req.params.owns_car;
+
+  let ownersFilteredArray = [];//array to push the matching objects to user's value
+
+  for (let i = 0; i < carOwners.length; i++) {
+    if (ownsParam.toLowerCase() === carOwners[i].owns_car.toLowerCase()) {
+      ownersFilteredArray.push(carOwners[i]);
+    }
+  }
+  res.send(ownersFilteredArray);
+
+
+});
+
+
+app.get('/carowners/fname=:first_name/ownscar=:owns_car',(req,res)=>{
+  const nameParam = req.params.first_name;
+  const carParam = req.params.owns_car; //retrieves the parameter value requested by the user
+
+  let ownersFilteredArray = [];//array to push the matching objects to user's value
+  for (let i = 0; i < carOwners.length; i++) {
+    if ((nameParam.toLowerCase() === carOwners[i].first_name.toLowerCase()) && (carParam.toLowerCase() === carOwners[i].owns_car.toLowerCase())){
+      ownersFilteredArray.push(carOwners[i]);
+    }
+  }
+  res.send(ownersFilteredArray);
+
+  });
 
 app.listen(port, () => console.log(`This app is listening on port ${port}!`))
